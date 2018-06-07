8ebdc7dac3cf2894b26a1ea158263ef7159bd7e7
lindaidai <244508795@qq.com>
2018-06-07 16:04:22 +0800

    add vue-temple

---
+++ vue-temple/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <router-link to="/demo-5">DemoFive</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import DemoFive from './components/DemoFive.vue'
+export default {
+  name: 'App',
+  components: {
+    DemoFive
+  },
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
